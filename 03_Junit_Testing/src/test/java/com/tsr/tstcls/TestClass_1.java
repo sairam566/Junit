aed2ddb3069654d621eb3b95beab84b0a22cee83
sairam566 <sairam1431995@gmail.com>
2024-01-28 14:49:02 +0530

    Test cases

---
+++ 03_Junit_Testing/src/test/java/com/tsr/tstcls/TestClass_1.java
@@ -0,0 +1,13 @@
+package com.tsr.tstcls;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestClass_1 {
+	
+	@Test
+	public void m1() {
+		assertTrue(true);
+	}
+}
---
+++ 03_Junit_Testing/src/test/java/com/tsr/tstcls/TestClass_3.java
@@ -0,0 +1,13 @@
+package com.tsr.tstcls;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestClass_3 {
+	
+	@Test
+	public void m1() {
+		assertTrue(true);
+	}
+}
