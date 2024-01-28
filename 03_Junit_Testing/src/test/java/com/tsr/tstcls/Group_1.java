70edfba4cbda10f6d821330b6bd31d43499aa1e4
sairam566 <sairam1431995@gmail.com>
2024-01-28 14:51:05 +0530

    Test Sute classes

---
+++ 03_Junit_Testing/src/test/java/com/tsr/tstcls/Group_1.java
@@ -0,0 +1,11 @@
+package com.tsr.tstcls;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestClass_1.class,TestClass_2.class})
+public class Group_1 {
+
+}
---
+++ 03_Junit_Testing/src/test/java/com/tsr/tstcls/Group_2.java
@@ -0,0 +1,11 @@
+package com.tsr.tstcls;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestClass_3.class,TestClass_4.class})
+public class Group_2 {
+
+}
