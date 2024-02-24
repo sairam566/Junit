a017a226718a142a213e5adea273531d864d1d31
sairam566 <sairam1431995@gmail.com>
2024-02-24 18:13:50 +0530

    SLF4j

---
+++ 06_SLF4J_Example/src/main/java/com/tsr/App.java
@@ -0,0 +1,18 @@
+package com.tsr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App 
+{	
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+    	LOGGER.trace("At TRACE Level-1");
+    	LOGGER.debug("At DEBUG Level");
+    	LOGGER.info("At INFO Level");
+    	LOGGER.warn("At WARN Level");
+    	LOGGER.error("At ERROR Level");
+    }
+}
